4bf091f2b2a282aa451b3be5d0340b3a2f8b9001
seomac <icyveins@hotmail.com>
2024-03-26 11:56:03 +0800

    recognise mdx as markdown for marksman lsp

--- init.lua
+++ init.lua
@@ -212,6 +212,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Enable mdx recognition as markdown
+vim.filetype.add {
+  extension = {
+    mdx = 'markdown',
+  },
+}
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
