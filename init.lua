19c81b2254f4ffbf182695b004cbca88588eb38f
icyveins7 <icyveins@hotmail.com>
2024-09-24 14:56:18 +0800

    add neogit

--- init.lua
+++ init.lua
@@ -937,6 +937,20 @@ require('lazy').setup({
       }
     end,
   },
+
+  -- Enable neogit for git?
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', --required
+      'sindrets/diffview.nvim', --optional -diff integration
+      -- Only one of these is needed
+      'nvim-telescope/telescope.nvim', -- optional
+      -- "ibhagwan/fzf-lua", --optional
+      -- "echasnovski/mini.pick", --optional
+    },
+    config = true,
+  },
 }, {
   concurrency = 1,
   ui = {
