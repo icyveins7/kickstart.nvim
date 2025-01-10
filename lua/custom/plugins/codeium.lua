21c364e4f5b360bc77e7d148c9440feed38cc5db
icyveins7 <icyveins@hotmail.com>
2025-01-10 15:00:16 +0800

    test with neocodeium instead?

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -15,14 +15,25 @@ return {
   --
   -- This doesn't seem to work. Just use the .vim one below
 
-  'Exafunction/codeium.vim',
-  event = 'BufEnter',
-  version = '1.8.37', -- seems like newer versions are bugged for now
-  config = function()
-    -- Set ctrl-t to call codeium#Accept
-    vim.keymap.set('i', '<C-t>', function()
-      return vim.fn['codeium#Accept']()
-    end, { expr = true, silent = true })
-  end,
+  -- 'Exafunction/codeium.vim',
+  -- event = 'BufEnter',
+  -- version = '1.8.37', -- seems like newer versions are bugged for now
+  -- config = function()
+  --   -- Set ctrl-t to call codeium#Accept
+  --   vim.keymap.set('i', '<C-t>', function()
+  --     return vim.fn['codeium#Accept']()
+  --   end, { expr = true, silent = true })
+  -- end,
+  --
+
+  {
+    'monkoose/neocodeium',
+    event = 'VeryLazy',
+    config = function()
+      local neocodeium = require 'neocodeium'
+      neocodeium.setup()
+      vim.keymap.set('i', '<C-t>', neocodeium.accept)
+    end,
+  },
 }
 -- Make sure you run ':Codeium Auth' after installation.
