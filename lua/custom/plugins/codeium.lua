ec9d6644bba4f3a812552b01796ff39a2c05dc38
icyveins7 <icyveins@hotmail.com>
2025-02-26 09:53:40 +0800

    set some codeium custom keymaps and use newest version again

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -21,15 +21,30 @@ return {
     'nvim-lua/plenary.nvim',
     'hrsh7th/nvim-cmp',
   },
-  commit = '289eb724e5d6fab2263e94a1ad6e54afebefafb2',
+  -- commit = '289eb724e5d6fab2263e94a1ad6e54afebefafb2',
   -- version = '1.8.37', -- seems like newer versions are bugged for now
   config = function()
+    -- disable defaults
+    vim.g.codeium_disable_bindings = 1
     -- Set ctrl-t to call codeium#Accept
     vim.keymap.set('i', '<C-t>', function()
       return vim.fn['codeium#Accept']()
     end, { expr = true, silent = true })
 
-    vim.g.codeium_log_file = '~/codeium.log'
+    -- these are not available until 1.8.58?
+    vim.keymap.set('i', '<C-l>', function()
+      return vim.fn['codeium#AcceptNextWord']()
+    end, { expr = true, silent = true })
+    vim.keymap.set('i', '<C-j>', function()
+      return vim.fn['codeium#AcceptNextLine']()
+    end, { expr = true, silent = true })
+
+    -- vim.g.codeium_log_file = '~/codeium.log'
+
+    vim.g.codeium_server_config = {
+      portal_url = vim.fn.getenv 'CODEIUM_PORTAL_URL',
+      api_url = vim.fn.getenv 'CODEIUM_API_URL',
+    }
   end,
 
   -- {
