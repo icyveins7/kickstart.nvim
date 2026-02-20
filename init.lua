eacb22bfbb545a954fc6e557e38e9bd98c63d96f
icyveins7 <icyveins@hotmail.com>
2026-02-20 15:07:07 +0800

    change markdownpreview to a maintained repo

--- init.lua
+++ init.lua
@@ -973,16 +973,28 @@ require('lazy').setup({
   },
 
   -- Enable markdown preview
+  -- {
+  --   'iamcco/markdown-preview.nvim',
+  --   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  --   build = 'cd app && npx yarn install',
+  --   init = function()
+  --     vim.g.mkdp_filetypes = { 'markdown' }
+  --     vim.g.mkdp_open_to_the_world = 1
+  --     vim.g.mkdp_echo_preview_url = 1
+  --   end,
+  --   ft = { 'markdown' },
+  -- },
   {
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    build = 'cd app && npx yarn install',
-    init = function()
-      vim.g.mkdp_filetypes = { 'markdown' }
-      vim.g.mkdp_open_to_the_world = 1
-      vim.g.mkdp_echo_preview_url = 1
+    'selimacerbas/markdown-preview.nvim',
+    dependencies = { 'selimacerbas/live-server.nvim' },
+    config = function()
+      require('markdown_preview').setup {
+        -- all optional; sane defaults shown
+        port = 8421,
+        open_browser = true,
+        debounce_ms = 300,
+      }
     end,
-    ft = { 'markdown' },
   },
 
   -- Enable doc generator
